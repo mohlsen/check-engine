ecf0592152ab7ec372c389b4bb8172c07b62c26d
Mike Ohlsen <mjohlsen@ra.rockwell.com>
2016-10-01 14:10:40 -0400

    colors and more validators

--- checker.js
+++ checker.js
@@ -1,25 +1,69 @@
 'use strict';
 
+const colors = require('colors');
 const exec = require('child_process').exec;
 const engines = require('./package.json').engines;
 const validaters =
 {
-  osx: {
-    versionCheck: 'sw_vers -productVersion',
-    versionValidate:
-      (detectedVersion, expectedVersion) => expectedVersion === detectedVersion.trim()
-  },
-  node: {
-    versionCheck: 'node -v',
-    versionValidate:
-      (detectedVersion, expectedVersion) =>  'v' + expectedVersion === detectedVersion.trim()
-  },
-  npm: {
-    versionCheck: 'npm -v',
-    versionValidate:
-      (detectedVersion, expectedVersion) =>  expectedVersion === detectedVersion.trim()
-  }
+    osx: {
+        versionCheck: 'sw_vers -productVersion',
+        versionValidate:
+            (detectedVersion, expectedVersion) => expectedVersion === detectedVersion.trim()
+    },
+    node: {
+        versionCheck: 'node -v',
+        versionValidate:
+            (detectedVersion, expectedVersion) =>  'v' + expectedVersion === detectedVersion.trim()
+    },
+    npm: {
+        versionCheck: 'npm -v',
+        versionValidate:
+            (detectedVersion, expectedVersion) =>  expectedVersion === detectedVersion.trim()
+    },
+    jx: {
+        versionCheck: 'jx -jxv',
+        versionValidate:
+            (result, version) =>  'v' + version === result.trim()
+    },
+    cordova: {
+        versionCheck: 'cordova -v',
+        versionValidate:
+            (result, version) =>  version === result.trim()
+    },
+    appium: {
+        versionCheck: 'appium -v',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    "ios-deploy": {
+        versionCheck: 'ios-deploy -V',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    "ios-sim": {
+        versionCheck: 'ios-sim --version',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    bower: {
+        versionCheck: 'bower -v',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    cordova: {
+        versionCheck: 'cordova -v',
+        versionValidate:
+            (result, version) => version === result.trim()
+    }
 }
+
+// set theme
+colors.setTheme({
+    success: 'green',
+    info: 'grey',
+    warn: 'yellow',
+    error: 'red'
+});
 
 if (!engines) {
     console.log("No engines found in package.json");
@@ -41,18 +85,21 @@ function runValidations() {
             let validator = validaters[name];
 
             if (validator === undefined) {
-                console.log(name + " was expected, but no validator found!  Aborting validation.")
-                process.exit(-1);
+                console.log(colors.warn(colors.bold(name) +
+                    " was expected, but no validator found!  Aborting validation."))
+                environmentValid = false;
+                return Promise.resolve();
             }
 
             //console.log("version " + engines[name]);
             //call the validator and pass in the version we expect
             return execAndCheck(validator, engines[name]).then((result) => {
-                console.log(name + " was validated with " + engines[name]);
+                console.log(colors.success("✔ " + colors.bold(name) + " was validated with " + engines[name]));
                 return Promise.resolve();
             })
             .catch((error) => {
-                console.log(name + " version is not correct! Expected: " + engines[name] + " but was " + error);
+                console.log(colors.error("✘ " + colors.bold(name) + " version is not correct! Expected: " + engines[name] +
+                    " but was " + error.trim()));
                 environmentValid = false;
                 return Promise.resolve(error);
             })
@@ -66,11 +113,11 @@ function runValidations() {
 
 runValidations()
     .then( () => {
-        console.log("Environment looks good!");
+        console.log(colors.success(colors.bold(colors.underline("Environment looks good!"))));
         process.exit(1);
     })
     .catch( () => {
-        console.log("Environment is invalid!");
+        console.log(colors.error(colors.bold(colors.underline("Environment is invalid!"))));
         process.exit(-1)
     });
 
