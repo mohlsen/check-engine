aed021b0e7c54a3deeacca8e242dea91d404fec5
Mike Ohlsen <mjohlsen@ra.rockwell.com>
2016-10-04 08:06:27 -0400

    more refactoring, pull out validators

--- checker.js
+++ checker.js
@@ -3,66 +3,8 @@
 const colors = require('colors');
 const exec = require('child_process').exec;
 const engines = require('./package.json').engines;
-let Promise = require("bluebird");
-
-const validaterRules =
-{
-    osx: {
-        versionCheck: 'sw_vers -productVersion',
-        versionValidate:
-            (detectedVersion, expectedVersion) => expectedVersion === detectedVersion.trim()
-    },
-    node: {
-        versionCheck: 'node -v',
-        versionValidate:
-            (detectedVersion, expectedVersion) =>  'v' + expectedVersion === detectedVersion.trim()
-    },
-    npm: {
-        versionCheck: 'npm -v',
-        versionValidate:
-            (detectedVersion, expectedVersion) =>  expectedVersion === detectedVersion.trim()
-    },
-    jx: {
-        versionCheck: 'jx -jxv',
-        versionValidate:
-            (result, version) =>  'v' + version === result.trim()
-    },
-    cordova: {
-        versionCheck: 'cordova -v',
-        versionValidate:
-            (result, version) =>  version === result.trim()
-    },
-    appium: {
-        versionCheck: 'appium -v',
-        versionValidate:
-            (result, version) => version === result.trim()
-    },
-    "ios-deploy": {
-        versionCheck: 'ios-deploy -V',
-        versionValidate:
-            (result, version) => version === result.trim()
-    },
-    "ios-sim": {
-        versionCheck: 'ios-sim --version',
-        versionValidate:
-            (result, version) => version === result.trim()
-    },
-    bower: {
-        versionCheck: 'bower -v',
-        versionValidate:
-            (result, version) => version === result.trim()
-    },
-    cordova: {
-        versionCheck: 'cordova -v',
-        versionValidate:
-            (result, version) => version === result.trim()
-    },
-    "ios-webkit-debug-proxy": {
-        versionCheck: 'ios-webkit-debug-proxy',
-        versionValidate:
-            (result, version) => version === result.trim()
-    }
-}
+const Promise = require("bluebird");
+const validaterRules = require('./validatorRules.js')
 
 // set theme
 colors.setTheme({
@@ -92,7 +34,7 @@ Promise.all(validators.map(function(promise) {
     } else {
         environmentValid = false;
     }
-}).then((test) => {
+}).then(() => {
     if (environmentValid) {
         console.log(colors.success(colors.bold(colors.underline("Environment looks good!"))));
     } else {
---
+++ validatorRules.js
@@ -0,0 +1,57 @@
+module.exports = {
+    osx: {
+        versionCheck: 'sw_vers -productVersion',
+        versionValidate:
+            (detectedVersion, expectedVersion) => expectedVersion === detectedVersion.trim()
+    },
+    node: {
+        versionCheck: 'node -v',
+        versionValidate:
+            (detectedVersion, expectedVersion) =>  'v' + expectedVersion === detectedVersion.trim()
+    },
+    npm: {
+        versionCheck: 'npm -v',
+        versionValidate:
+            (detectedVersion, expectedVersion) =>  expectedVersion === detectedVersion.trim()
+    },
+    jx: {
+        versionCheck: 'jx -jxv',
+        versionValidate:
+            (result, version) =>  'v' + version === result.trim()
+    },
+    cordova: {
+        versionCheck: 'cordova -v',
+        versionValidate:
+            (result, version) =>  version === result.trim()
+    },
+    appium: {
+        versionCheck: 'appium -v',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    "ios-deploy": {
+        versionCheck: 'ios-deploy -V',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    "ios-sim": {
+        versionCheck: 'ios-sim --version',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    bower: {
+        versionCheck: 'bower -v',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    cordova: {
+        versionCheck: 'cordova -v',
+        versionValidate:
+            (result, version) => version === result.trim()
+    },
+    "ios-webkit-debug-proxy": {
+        versionCheck: 'ios-webkit-debug-proxy',
+        versionValidate:
+            (result, version) => version === result.trim()
+    }
+}
