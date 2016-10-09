46aec9abc6212a8fd33cefd18280aa66c13e68b7
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-10-09 12:27:38 -0400

    added a utility that helps with creating test package files

---
+++ test/MockFile.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function MockFile(filePath, fileContents) {
+    this.path = filePath;
+    this.contents = fileContents;
+
+    try {
+        fs.writeFileSync(this.path, JSON.stringify(this.contents));
+        this.status = 0;
+        return this;
+    }
+    catch (ex) {
+        this.status = -1;
+        this.error = ex;
+        return this;
+    }
+}
+
+MockFile.prototype.delete = function() {
+    try {
+        fs.unlinkSync(this.path);
+        this.deleted = true;
+        return { status: 0 };
+    }
+    catch (ex) {
+        return { status: -1, error: ex };
+    }
+};
+
+module.exports = MockFile;
